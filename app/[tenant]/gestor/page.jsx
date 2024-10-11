189df931a9501bc06f0af54c3c57303e01d6519e
Aletho Alves <alethoalves@gmail.com>
2024-10-11 20:54:26 -0300

    PLIC 0.4.15-beta - Add loading no botão de download do excel

--- app/[tenant]/gestor/page.jsx
+++ app/[tenant]/gestor/page.jsx
@@ -3,7 +3,6 @@ import styles from "./page.module.scss";
 import { RiFileExcelLine } from "@remixicon/react";
 
 import Button from "@/components/Button";
-import { useState, useEffect } from "react";
 import ExcelJS from "exceljs"; // Para exportação do Excel
 import { saveAs } from "file-saver"; // Para salvar o arquivo Excel
 import Inscricoes from "@/components/dashboards/Inscricoes";
@@ -11,113 +10,113 @@ import Participacoes from "@/components/dashboards/Participacoes";
 import Atividades from "@/components/dashboards/Atividades";
 import Eventos from "@/components/dashboards/Eventos";
 import { relatorioInscricoes } from "@/app/api/client/relatorios";
+import { useState } from "react";
 
 const Page = ({ params }) => {
-  const [alunos, setAlunos] = useState([]);
-
-  // Função para buscar dados dos alunos da API
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const participacoes = await relatorioInscricoes(params.tenant);
-        setAlunos(participacoes.alunos || []);
-      } catch (error) {
-        console.error("Erro ao obter os dados:", error);
-      }
-    };
-
-    fetchData();
-  }, [params.tenant]);
+  const [isDownloading, setIsDownloading] = useState(false);
 
   // Função para exportar os dados para Excel
   const exportToExcel = async () => {
-    const workbook = new ExcelJS.Workbook();
-    const worksheet = workbook.addWorksheet("Relatório de Participações");
+    setIsDownloading(true); // Inicia o processo de download e desativa o botão
 
-    // Definir as colunas do Excel
-    worksheet.columns = [
-      { header: "ID Inscrição", key: "inscricao_id", width: 15 },
-      { header: "Status", key: "status", width: 15 },
-      { header: "Tipo", key: "tipo", width: 15 },
-      { header: "Início", key: "inicio", width: 15 },
-      { header: "Fim", key: "fim", width: 15 },
-      { header: "Edital", key: "edital", width: 25 },
-      { header: "Ano do Edital", key: "edital_ano", width: 15 },
-      { header: "Aluno", key: "aluno", width: 25 },
-      { header: "Email", key: "email", width: 25 },
-      { header: "CPF", key: "cpf", width: 20 },
-      { header: "Plano", key: "plano", width: 40 },
-      { header: "Área", key: "area", width: 20 },
-      { header: "Grande Área", key: "grandeArea", width: 25 },
-      {
-        header: "Atividades Concluídas",
-        key: "total_atividades_concluidas",
-        width: 20,
-      },
-      {
-        header: "Atividades Pendentes",
-        key: "total_atividades_pendentes",
-        width: 20,
-      },
-      { header: "Inscrição em Eventos", key: "inscricao_eventos", width: 20 },
-    ];
+    try {
+      // Buscar dados dos alunos da API apenas quando o botão for clicado
+      const participacoes = await relatorioInscricoes(params.tenant);
+      const alunos = participacoes.alunos || []; // Obtém os dados diretamente aqui
 
-    // Adicionar as linhas com os dados dos alunos
-    alunos.forEach((aluno) => {
-      worksheet.addRow({
-        inscricao_id: aluno.inscricao_id,
-        status: aluno.status,
-        tipo: aluno.tipo,
-        inicio: aluno.inicio,
-        fim: aluno.fim || "N/A",
-        edital: aluno.edital,
-        edital_ano: aluno.edital_ano,
-        aluno: aluno.aluno,
-        email: aluno.email || "N/A",
-        cpf: aluno.cpf,
-        plano: aluno.plano,
-        area: aluno.area || "N/A",
-        grandeArea: aluno.grandeArea || "N/A",
-        total_atividades_concluidas: aluno.total_atividades_concluidas,
-        total_atividades_pendentes: aluno.total_atividades_pendentes,
-        inscricao_eventos: aluno.inscricao_eventos,
+      // Criar workbook e worksheet para o Excel
+      const workbook = new ExcelJS.Workbook();
+      const worksheet = workbook.addWorksheet("Relatório de Participações");
+
+      // Definir as colunas do Excel
+      worksheet.columns = [
+        { header: "ID Inscrição", key: "inscricao_id", width: 15 },
+        { header: "Status", key: "status", width: 15 },
+        { header: "Tipo", key: "tipo", width: 15 },
+        { header: "Início", key: "inicio", width: 15 },
+        { header: "Fim", key: "fim", width: 15 },
+        { header: "Edital", key: "edital", width: 25 },
+        { header: "Ano do Edital", key: "edital_ano", width: 15 },
+        { header: "Aluno", key: "aluno", width: 25 },
+        { header: "Email", key: "email", width: 25 },
+        { header: "CPF", key: "cpf", width: 20 },
+        { header: "Plano", key: "plano", width: 40 },
+        { header: "Área", key: "area", width: 20 },
+        { header: "Grande Área", key: "grandeArea", width: 25 },
+        {
+          header: "Atividades Concluídas",
+          key: "total_atividades_concluidas",
+          width: 20,
+        },
+        {
+          header: "Atividades Pendentes",
+          key: "total_atividades_pendentes",
+          width: 20,
+        },
+        { header: "Inscrição em Eventos", key: "inscricao_eventos", width: 20 },
+      ];
+
+      // Adicionar as linhas com os dados dos alunos
+      alunos.forEach((aluno) => {
+        worksheet.addRow({
+          inscricao_id: aluno.inscricao_id,
+          status: aluno.status,
+          tipo: aluno.tipo,
+          inicio: aluno.inicio,
+          fim: aluno.fim || "N/A",
+          edital: aluno.edital,
+          edital_ano: aluno.edital_ano,
+          aluno: aluno.aluno,
+          email: aluno.email || "N/A",
+          cpf: aluno.cpf,
+          plano: aluno.plano,
+          area: aluno.area || "N/A",
+          grandeArea: aluno.grandeArea || "N/A",
+          total_atividades_concluidas: aluno.total_atividades_concluidas,
+          total_atividades_pendentes: aluno.total_atividades_pendentes,
+          inscricao_eventos: aluno.inscricao_eventos,
+        });
       });
-    });
 
-    // Adicionar uma tabela com os dados
-    worksheet.addTable({
-      name: "RelatorioParticipacoes",
-      ref: "A1", // Referência onde começa a tabela (canto superior esquerdo)
-      headerRow: true,
-      columns: worksheet.columns.map((col) => ({ name: col.header })),
-      rows: alunos.map((aluno) => [
-        aluno.inscricao_id,
-        aluno.status,
-        aluno.tipo,
-        aluno.inicio,
-        aluno.fim || "N/A",
-        aluno.edital,
-        aluno.edital_ano,
-        aluno.aluno,
-        aluno.email || "N/A",
-        aluno.cpf,
-        aluno.plano,
-        aluno.area || "N/A",
-        aluno.grandeArea || "N/A",
-        aluno.total_atividades_concluidas,
-        aluno.total_atividades_pendentes,
-        aluno.inscricao_eventos,
-      ]),
-    });
+      // Adicionar uma tabela com os dados
+      worksheet.addTable({
+        name: "RelatorioParticipacoes",
+        ref: "A1", // Referência onde começa a tabela (canto superior esquerdo)
+        headerRow: true,
+        columns: worksheet.columns.map((col) => ({ name: col.header })),
+        rows: alunos.map((aluno) => [
+          aluno.inscricao_id,
+          aluno.status,
+          aluno.tipo,
+          aluno.inicio,
+          aluno.fim || "N/A",
+          aluno.edital,
+          aluno.edital_ano,
+          aluno.aluno,
+          aluno.email || "N/A",
+          aluno.cpf,
+          aluno.plano,
+          aluno.area || "N/A",
+          aluno.grandeArea || "N/A",
+          aluno.total_atividades_concluidas,
+          aluno.total_atividades_pendentes,
+          aluno.inscricao_eventos,
+        ]),
+      });
 
-    // Gerar arquivo Excel
-    const buffer = await workbook.xlsx.writeBuffer();
-    const blob = new Blob([buffer], {
-      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-    });
+      // Gerar arquivo Excel
+      const buffer = await workbook.xlsx.writeBuffer();
+      const blob = new Blob([buffer], {
+        type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      });
 
-    // Salvar o arquivo
-    saveAs(blob, "relatorio_participacoes_alunos.xlsx");
+      // Salvar o arquivo
+      saveAs(blob, "relatorio_participacoes_alunos.xlsx");
+    } catch (error) {
+      console.error("Erro ao obter os dados ou gerar o Excel:", error);
+    } finally {
+      setIsDownloading(false); // Finaliza o processo de download e reativa o botão
+    }
   };
 
   return (
@@ -128,8 +127,9 @@ const Page = ({ params }) => {
           icon={RiFileExcelLine}
           className="btn-green"
           type="button"
+          disabled={isDownloading} // Desabilita o botão enquanto o download está em andamento
         >
-          Exportar Excel
+          {isDownloading ? "Exportando..." : "Exportar Excel"}
         </Button>
       </div>
 
